ccd2a2af3d51f73eb4aea2bfd0f02eb92873305a
hilaryberigan <berigan.hilary@gmail.com>
2016-08-26 12:31:11 -0500

    initial classes setup

---
+++ HumaneSocietyDatabase/HumaneSocietyModelingLib/GeneratedCode/Adopter.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Adopter
+{
+    string name;
+    bool hasPaid;
+    decimal moneyOwed;
+   
+    public Adopter()
+    {
+        
+    }
+    public string Name
+	{
+		get { return name; }
+ 		set { name = value; }
+	}
+	public bool HasPaid
+	{
+        get { return hasPaid; }
+    }
+    public decimal MoneyOwed
+    {
+        get { return moneyOwed; }
+        set { moneyOwed = value;
+            if (value == 0)
+            {
+                hasPaid = true;
+            }
+        }
+    }
+
+	public void AdoptAnimal()
+	{
+
+		
+	}
+    public void PayForAnimal()
+    {
+
+    }
+
+	
+
+}
+
---
+++ HumaneSocietyDatabase/HumaneSocietyModelingLib/GeneratedCode/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSocietyModelingLib.GeneratedCode
+{
+    class Database
+    {
+        Menu menu = new Menu();
+        HumaneSociety HumaneSociety = new HumaneSociety();
+
+        public void AccessDatabase()
+        {
+
+        }
+    }
+}
